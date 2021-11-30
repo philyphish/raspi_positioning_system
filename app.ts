ca31d9e41526d9a21465c3ca40379e5555927922
philyphish <philipknigh@gmail.com>
2021-11-30 13:04:25 -0600

    removes trigger route

--- app.ts
+++ app.ts
@@ -9,41 +9,45 @@ const publicPath = express.static(path.join(__dirname, "../client/build"), {
 const Gpio = require("pigpio").Gpio;
 const triggersRoute = require("./routes/trigger");
 
+app.get("/test", (req, res) => {
+  res.send("this is the root");
+
+  // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
+  const MICROSECDONDS_PER_CM = 1e6 / 34321;
+
+  const trigger = new Gpio(7, { mode: Gpio.OUTPUT });
+  const echo = new Gpio(11, { mode: Gpio.INPUT, alert: true });
+
+  trigger.digitalWrite(0); // Make sure trigger is low
+
+  const watchHCSR04 = () => {
+    let startTick: any;
+
+    echo.on("alert", (level: any, tick: any) => {
+      if (level == 1) {
+        startTick = tick;
+      } else {
+        const endTick = tick;
+        const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+        console.log(`tick is type of: ${typeof tick}`);
+        console.log(diff / 2 / MICROSECDONDS_PER_CM);
+      }
+    });
+  };
+
+  watchHCSR04();
+
+  // Trigger a distance measurement once per second
+  setInterval(() => {
+    trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+  }, 1000);
+});
+
 app.use(publicPath);
 
 app.listen(port, () => {
   console.log(`Server Listening on port ${port}`);
 });
-
-// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-const MICROSECDONDS_PER_CM = 1e6 / 34321;
-
-const trigger = new Gpio(7, { mode: Gpio.OUTPUT });
-const echo = new Gpio(11, { mode: Gpio.INPUT, alert: true });
-
-trigger.digitalWrite(0); // Make sure trigger is low
-
-const watchHCSR04 = () => {
-  let startTick: any;
-
-  echo.on("alert", (level: any, tick: any) => {
-    if (level == 1) {
-      startTick = tick;
-    } else {
-      const endTick = tick;
-      const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-      console.log(`tick is type of: ${typeof tick}`);
-      console.log(diff / 2 / MICROSECDONDS_PER_CM);
-    }
-  });
-};
-
-watchHCSR04();
-
-// Trigger a distance measurement once per second
-setInterval(() => {
-  trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-}, 1000);
 
 ///////// GPIO PINS FOR HC-SR04 /////////////////
 // VCC Connects to Pin 2 (5v)
--- dist/routes/trigger.js
+++ dist/routes/trigger.js
@@ -7,40 +7,8 @@ const express_1 = __importDefault(require("express"));
 const express_2 = require("express");
 const app = (0, express_1.default)();
 const router = (0, express_2.Router)();
-const Gpio = require("pigpio").Gpio;
 router.get("/", () => {
     console.log('triggers.route');
-    // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-    const MICROSECDONDS_PER_CM = 1e6 / 34321;
-    const trigger = new Gpio(7, { mode: Gpio.OUTPUT });
-    const echo = new Gpio(11, { mode: Gpio.INPUT, alert: true });
-    trigger.digitalWrite(0); // Make sure trigger is low
-    const watchHCSR04 = () => {
-        let startTick;
-        echo.on("alert", (level, tick) => {
-            if (level == 1) {
-                startTick = tick;
-            }
-            else {
-                const endTick = tick;
-                const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-                console.log(`tick is type of: ${typeof tick}`);
-                console.log(diff / 2 / MICROSECDONDS_PER_CM);
-            }
-        });
-    };
-    watchHCSR04();
-    // Trigger a distance measurement once per second
-    setInterval(() => {
-        trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-    }, 1000);
 });
-///////// GPIO PINS FOR HC-SR04 /////////////////
-// VCC Connects to Pin 2 (5v)
-// Trig Connects to Pin 7 (GPIO 4)
-// Echo Connects to R1 (1k Ω)
-// R2 (1k Ω) Connects from R1 to Ground
-// Wire from R1 and R2 connects to Pin 11
-// GND connects to Pin 6 (Ground)
 module.exports = router;
 //# sourceMappingURL=trigger.js.map
