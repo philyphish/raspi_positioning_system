55f85ca6d20fef6c09bce2d024a244c71e543fb8
philyphish <philipknigh@gmail.com>
2021-11-30 13:32:24 -0600

    switches gpio numbers

--- app.ts
+++ app.ts
@@ -15,8 +15,8 @@ app.get("/test", (req, res) => {
   // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
   const MICROSECDONDS_PER_CM = 1e6 / 34321;
 
-  const trigger = new Gpio(7, { mode: Gpio.OUTPUT });
-  const echo = new Gpio(11, { mode: Gpio.INPUT, alert: true });
+  const trigger = new Gpio(4, { mode: Gpio.OUTPUT });
+  const echo = new Gpio(17, { mode: Gpio.INPUT, alert: true });
 
   trigger.digitalWrite(0); // Make sure trigger is low
 
